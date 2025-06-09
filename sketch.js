8d7211398203958d6f900434741dae046e901ccf
Mónica Gómez <m0n1c4g0m3z@protonmail.ch>
2025-06-09 23:58:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,47 @@
-let datos;
-let artistaActual = 0;
+JSONObject autoras;
+String teclaActual = "";
+int cantidad = 0;
+color formaColor;
+String forma = "";
 
-function preload() {
-  datos = loadJSON('data.json');
+void setup() {
+  size(800, 600);
+  background(0);
+  autoras = loadJSONObject("data.json");
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER, CENTER);
-  textSize(32);
-}
+void draw() {
+  background(0);  // Fondo negro mate cada frame
 
-function draw() {
-  background(30);
-  fill(255);
-  if (datos && datos.artistas && datos.artistas.length > 0) {
-    text(datos.artistas[artistaActual].nombre, width / 2, height / 2);
+  if (teclaActual != "") {
+    JSONObject autora = autoras.getJSONObject(teclaActual);
+    cantidad = autora.getInt("cantidad");
+    formaColor = color(autora.getJSONArray("color").getInt(0),
+                       autora.getJSONArray("color").getInt(1),
+                       autora.getJSONArray("color").getInt(2));
+    forma = autora.getString("forma");
+
+    fill(formaColor);
+    noStroke();
+
+    for (int i = 0; i < cantidad; i++) {
+      float x = random(width);
+      float y = random(height);
+      float s = random(20, 60);
+
+      if (forma.equals("circle")) {
+        ellipse(x, y, s, s);
+      } else if (forma.equals("rect")) {
+        rect(x, y, s, s);
+      } else if (forma.equals("triangle")) {
+        triangle(x, y, x + s, y, x + s/2, y - s);
+      }
+    }
   }
 }
 
-function keyPressed() {
-  artistaActual = (artistaActual + 1) % datos.artistas.length;
+void keyPressed() {
+  if (key >= 'a' && key <= 'm') {
+    teclaActual = str(key);
+  }
 }

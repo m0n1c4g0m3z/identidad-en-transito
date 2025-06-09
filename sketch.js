c45fe32a6c63018c09cebce946c07bcdaff7c8c6
Mónica Gómez <m0n1c4g0m3z@protonmail.ch>
2025-06-09 23:53:39 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,24 @@
-
-let artistas;
-let actual = 0;
+let datos;
+let artistaActual = 0;
 
 function preload() {
-  artistas = loadJSON('data.json');
+  datos = loadJSON('data.json');
 }
 
 function setup() {
-  createCanvas(600, 400);
-  noStroke();
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  textSize(32);
 }
 
 function draw() {
-  background(20);
-  let art = artistas.artistas[actual];
-  fill(art.color);
-
-  for (let i = 0; i < art.intensidad * 10; i++) {
-    let x = random(width);
-    let y = random(height);
-    switch (art.forma) {
-      case "circulo":
-        ellipse(x, y, 30, 30);
-        break;
-      case "cuadrado":
-        rect(x, y, 30, 30);
-        break;
-      case "linea":
-        line(x, y, x + 20, y + 20);
-        break;
-    }
+  background(30);
+  fill(255);
+  if (datos && datos.artistas && datos.artistas.length > 0) {
+    text(datos.artistas[artistaActual].nombre, width / 2, height / 2);
   }
 }
 
 function keyPressed() {
-  if (key === '1') actual = 0;
-  if (key === '2') actual = 1;
-  if (key === '3') actual = 2;
+  artistaActual = (artistaActual + 1) % datos.artistas.length;
 }

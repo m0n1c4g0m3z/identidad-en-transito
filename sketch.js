c4148cb2b2d76cf85226d5bed865bcd925171e0b
Mónica Gómez <m0n1c4g0m3z@protonmail.ch>
2025-06-10 00:06:29 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let currentKey = null;
 
 function preload() {
   // Cargar el JSON con la información de las autoras
-  data = loadJSON("demo_data.json");
+  data = loadJSON("demo.json");
 }
 
 function setup() {

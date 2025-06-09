fb2c8b1bc401f32d9917c2b64f6823a0b2eb0d24
Mónica Gómez <m0n1c4g0m3z@protonmail.ch>
2025-06-10 00:03:20 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,47 +1,49 @@
-JSONObject autoras;
-String teclaActual = "";
-int cantidad = 0;
-color formaColor;
-String forma = "";
+let data;
+let currentKey = null;
 
-void setup() {
-  size(800, 600);
-  background(0);
-  autoras = loadJSONObject("data.json");
+function preload() {
+  // Cargar el JSON con la información de las autoras
+  data = loadJSON("demo_data.json");
 }
 
-void draw() {
-  background(0);  // Fondo negro mate cada frame
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
+}
 
-  if (teclaActual != "") {
-    JSONObject autora = autoras.getJSONObject(teclaActual);
-    cantidad = autora.getInt("cantidad");
-    formaColor = color(autora.getJSONArray("color").getInt(0),
-                       autora.getJSONArray("color").getInt(1),
-                       autora.getJSONArray("color").getInt(2));
-    forma = autora.getString("forma");
+function draw() {
+  background(0, 20); // Fondo negro con leve transparencia para efecto trazo
 
-    fill(formaColor);
-    noStroke();
+  if (currentKey && data[currentKey]) {
+    let props = data[currentKey];
+    fill(props.color[0], props.color[1], props.color[2], 150);
 
-    for (int i = 0; i < cantidad; i++) {
-      float x = random(width);
-      float y = random(height);
-      float s = random(20, 60);
+    for (let i = 0; i < props.count; i++) {
+      let x = random(width);
+      let y = random(height);
+      let s = random(props.size[0], props.size[1]);
 
-      if (forma.equals("circle")) {
-        ellipse(x, y, s, s);
-      } else if (forma.equals("rect")) {
+      // Según la forma
+      if (props.form === "circle") {
+        ellipse(x, y, s);
+      } else if (props.form === "square") {
         rect(x, y, s, s);
-      } else if (forma.equals("triangle")) {
-        triangle(x, y, x + s, y, x + s/2, y - s);
+      } else if (props.form === "triangle") {
+        triangle(x, y, x + s, y, x + s / 2, y - s);
+      } else if (props.form === "line") {
+        stroke(props.color[0], props.color[1], props.color[2]);
+        line(x, y, x + s, y + s);
+        noStroke();
       }
+      // Puedes seguir añadiendo más formas si lo necesitas
     }
   }
 }
 
-void keyPressed() {
-  if (key >= 'a' && key <= 'm') {
-    teclaActual = str(key);
+function keyPressed() {
+  let keyUpper = key.toUpperCase();
+  if (data[keyUpper]) {
+    currentKey = keyUpper;
   }
 }

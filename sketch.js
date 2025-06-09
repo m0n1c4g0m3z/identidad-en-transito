35277fdd5a636a3500a6ad3c1acef78b70d06676
Mónica Gómez <m0n1c4g0m3z@protonmail.ch>
2025-06-10 00:52:28 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
-let data;
+let data = [];
 let currentKey = null;
 
 function preload() {
-  // Cargar el JSON con la información de las autoras
   data = loadJSON("data.json");
 }
 
@@ -13,37 +12,35 @@ function setup() {
 }
 
 function draw() {
-  background(0, 20); // Fondo negro con leve transparencia para efecto trazo
+  background(0, 20);
 
-  if (currentKey && data[currentKey]) {
-    let props = data[currentKey];
-    fill(props.color[0], props.color[1], props.color[2], 150);
+  if (currentKey) {
+    let props = data.find(entry => entry.key === currentKey);
+    if (props) {
+      fill(props.color[0], props.color[1], props.color[2], 150);
 
-    for (let i = 0; i < 10; i++) {
-      let x = random(width);
-      let y = random(height);
-      let s = props.size;
+      for (let i = 0; i < 10; i++) {
+        let x = random(width);
+        let y = random(height);
+        let s = props.size;
 
-      // Según la forma
-      if (props.form === "circle") {
-        ellipse(x, y, s);
-      } else if (props.form === "square") {
-        rect(x, y, s, s);
-      } else if (props.form === "triangle") {
-        triangle(x, y, x + s, y, x + s / 2, y - s);
-      } else if (props.form === "line"){
-        stroke(props.color[0], props.color[1], props.color[2]);
-        line(x, y, x + s, y + s);
-        noStroke();
+        if (props.shape === "circle") {
+          ellipse(x, y, s);
+        } else if (props.shape === "square") {
+          rect(x, y, s, s);
+        } else if (props.shape === "triangle") {
+          triangle(x, y, x + s, y, x + s / 2, y - s);
+        } else if (props.shape === "line") {
+          stroke(props.color[0], props.color[1], props.color[2]);
+          line(x, y, x + s, y + s);
+          noStroke();
+        }
+        // Puedes añadir más formas personalizadas aquí según el JSON
       }
-      // Puedes añadir más formas personalizadas aquí si las defines en el JSON
     }
   }
 }
 
 function keyPressed() {
-  let keyLower = key.toLowerCase(); // convierte la tecla a minúscula
-  if (data[keyLower]) {
-    currentKey = keyLower;
-  }
+  currentKey = key.toLowerCase();
 }
